bb689b40ea02858195e1f5f15fa38d6741d2dd06
zhangsz <958961611@qq.com>
2018-10-13 00:15:11 +0800

    添加Redis配置类

---
+++ document/src/main/java/com/zsz/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.zsz;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+
+/**
+ * @author zhangsz
+ * @ClassName: RedisConfig
+ * @Description: Redis配置类(这里用一句话描述这个类的作用)
+ * @date 2018-10-12 23:49
+ * @Version 1.0
+ */
+public class RedisConfig extends CachingConfigurerSupport {
+}   
